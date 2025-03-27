ffd89a93b7b4dab0d68676f49e065e522dcb61b7
diogo <diogo@DESKTOP-N9C1TD3>
2025-03-27 01:05:11 -0300

    Adicionar arquivos de projeto.

---
+++ Auth/CustomAuthStateProvider.cs
@@ -0,0 +1,31 @@
+﻿using BlazorAuthAPI.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorAuthAPI.Auth
+{
+    public class CustomAuthStateProvider(UserState userState) : AuthenticationStateProvider
+    {
+        private readonly UserState _userState = userState;
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            ClaimsIdentity identity = new ClaimsIdentity();
+            if (_userState.IsAutenticated && _userState.Username != null)
+            {
+                identity = new ClaimsIdentity(
+                [
+                new Claim(ClaimTypes.Name, _userState.Username),
+            ], "apiauth_type");
+            }
+            ClaimsPrincipal user = new ClaimsPrincipal(identity);
+            return Task.FromResult(new AuthenticationState(user));
+        }
+
+        public void AuthenticateUser(string userName)
+        {
+            _userState.Username = userName;
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
---
+++ Models/UserState.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAuthAPI.Models
+{
+    public class UserState
+    {
+        public string? Username { get; set; }
+        public string? SessionID { get; set; }
+        public bool IsAutenticated { get; set; }
+    }
+}
